7d5bc8867eed30da876c141e3b766fa3c8e9e0e9
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-01-12 00:05:27 +0500

    add styling to the app

--- db/migrate/20240110151022_create_recipes.rb
+++ db/migrate/20240110151022_create_recipes.rb
@@ -1,13 +1,14 @@
 class CreateRecipes < ActiveRecord::Migration[7.1]
   def change
     create_table :recipes do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
-    t.boolean "is_public" 
-    t.references :user, foreign_key: true
-    t.timestamps
+      t.string :name
+      t.integer :preparation_time
+      t.integer :cooking_time
+      t.text :description
+      t.boolean :public, default: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
     end
   end
 end
